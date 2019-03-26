501ad1a04044711cd8b0f10b3d936f1fe93f604c
dsanderson90 <dsanderson3@cnm.edu>
2019-03-26 16:30:51 -0600

    first commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php wp_head();?>
+    
+    <div class="container">
+
+        <div class="row">
+    
+            <div class="col">
+            Left Side
+            </div>
+
+            <div class="col">
+            Right Side
+            </div>
+    
+        </div>
+        
+    </div>
+
+</head>
+<body <?php body_class();?>>
